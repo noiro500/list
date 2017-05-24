22516989584deefb1f995b9563cf55e32eff630c
noiro <noiro@NOIRO-PC>
2017-05-24 20:23:19 +0300

    123

--- listApp/listApp/Program.cs
+++ listApp/listApp/Program.cs
@@ -74,19 +74,47 @@ namespace list
                 list.Insert(0, new Node<T>(value));
                 list[0].vNext = list[1];
             }
+
+            public void ReverseList()
+            {
+                Node<T> current=list[0];
+
+                list[0] = list[list.Count-1];
+                list[list.Count-1] = current;
+
+                current = list[list.Count-1];
+                while (current.vNext != null)
+                {
+                    current = current.vNext;
+                }
+                
+            }
+
+            public void ShowList()
+            {
+                Node<T> current = list[0];
+                while (current.vNext!=null)
+                {
+                    Console.Write(current.Data+" ");
+                    current = current.vNext;
+                }
+                //для вывода последнего значения
+                Console.Write(current.Data + " ");
+                Console.WriteLine();
+            }
         }
+
         static void Main(string[] args)
         {
             //Stopwatch stopwatch = new Stopwatch();
             //stopwatch.Start();
-
+            Random rand=new Random();
             LinkList<int> linkList = new LinkList<int>();
             for (int i = 1; i < 11; i++)
-                linkList.AddLast(i);
-            //stopwatch.Stop();
-            //Console.WriteLine(stopwatch.ElapsedTicks);
-            //linkList.Remove(1);
-            linkList.AppendFirst(66);
+                linkList.AddLast(rand.Next(1000));
+
+            linkList.ShowList();
+            linkList.ReverseList();
         }
     }
 }
